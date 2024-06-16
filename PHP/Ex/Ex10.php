664c48362190222b37ff366b6b6b78d0f7a2faa9
congtien2001 <tien1972001@gmail.com>
2024-06-16 18:49:41 +0700

    16/06

--- PHP/Ex/Ex10.php
+++ PHP/Ex/Ex10.php
@@ -13,13 +13,23 @@
     echo "<br>";
     echo $a % $b;
     echo "<br>";
+    //toán tử gán (+=, -=, *=, /=, %=)
+    echo $a += $b;
+    echo "<br>";
     //toán tử so sánh (==, >=, <=, <, >, !=)
     if ($a == $b) {
         echo "a bang b";
     } else {
         echo "a khong bang b";
+        echo"<br>";
     }
     //toán tử tăng giảm
-    
+    echo ++$a;//cộng trước rồi hiển thị
+    echo --$a;//trừ trc rồi hiển thị
+    echo "<br>";
+    echo $a++;//hiển thị rồi cộng
+    echo $a--;//hiện thị rồi trừ
+    echo "<br>";
+    //toán tử logic (&&, ||,!)
 
 ?>

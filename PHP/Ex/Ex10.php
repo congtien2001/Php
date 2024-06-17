fb6a791ab27c3c44b7ccef5ba88a05be3856da00
congtien2001 <tien1972001@gmail.com>
2024-06-17 11:00:39 +0700

    update 17/06

--- PHP/Ex/Ex10.php
+++ PHP/Ex/Ex10.php
@@ -2,6 +2,10 @@
     //khai báo biến
     $a = 10;
     $b = 5;
+    $name = "Nguyen Cong Tien tuoi: ";
+    $age = 23;
+    $nameAndAge = $name . $age;
+    
     //toán tử số học
     echo $a + $b;
     echo "<br>";
@@ -30,6 +34,30 @@
     echo $a++;//hiển thị rồi cộng
     echo $a--;//hiện thị rồi trừ
     echo "<br>";
-    //toán tử logic (&&, ||,!)
-
+    //toán tử logic (&&(and), ||(or),!(ngược lại kq), ^(xor))
+    if ($a > 10 &&  $b < 10) {
+        echo "và";
+        echo "<br>";
+    }
+     if ($a > 10 || $b < 10) {
+        echo "hoặc";
+        echo "<br>";
+    }
+    if ($a > 10 or $b < 10) {
+        echo "!";
+        echo "<br>";
+    }
+    if ($a > 5 xor $b > 8) {// lưu ý
+        echo "dấu mũ";
+        echo "<br>";
+    }
+    //toán tử chuỗi
+    echo $name.$age;
+    echo "<br>";
+    echo $nameAndAge;
+    echo "<br>";
+    echo $name .= $age;// cũng là nối chuỗi
+    echo "<br>";
+    //toán tử mảng
+    //toán tử gán có điều kiện
 ?>

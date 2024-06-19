7f387e2bd35e1ab5dc222085c60d4315d061d759
congtien2001 <tien1972001@gmail.com>
2024-06-19 10:34:00 +0700

    update 19/06

---
+++ PHP/Ex/Ex11.php
@@ -0,0 +1,17 @@
+<?php
+    $color = "màu đỏ";
+    //câu lệnh 
+    switch ($color) {
+        //điều kiện
+        case "màu đỏ";
+            echo"this is đỏ";
+            //dừng
+            break;
+        case "màu xanh";
+            echo"this is xanh";
+            break;
+        case "màu đen";
+            echo"this is đen";
+            break;
+    }
+?>

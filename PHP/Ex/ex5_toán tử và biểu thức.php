e45adecd09b9cce1745600078d81e4d068b53191
congtien2001 <tien1972001@gmail.com>
2024-07-03 19:52:19 +0700

    03/07

--- PHP/Ex/ex5_toán tử và biểu thức.php
+++ PHP/Ex/ex5_toán tử và biểu thức.php
@@ -1,3 +1,35 @@
 <?php 
+    $bien1 = 5;
 
+    $bien2 = 6;
+    $bien2 += 10;// $bien2 = $bien2 +10;
+    echo 'Biến số 2 = '. $bien2. '<br>';
+
+    $bien3 = 9;
+    $bien3 -= 6;// $bien3 = $bien3 - 6;
+    echo 'Biến số 3 = '. $bien3. '<br>';
+
+    $bien4 = 10;
+    $bien4 *= 10;
+    echo 'Biến số 4 = '. $bien4.'<br>';
+
+    $bien5 = 9;
+    $bien5 /= 3;
+    echo 'Biến số 5 = '.$bien5.'<br>';
+    //chia lấy dư
+    $bien6 = 10;
+    $bien6 %= 3;// $bien6 = $bien6 % 7;
+    echo 'Biến số 6 = '. $bien6.'<br>';
+    //toán tử gán  .=
+    $bien7 = 'Storm ';
+    $bien7 .= 'Nguyen Cong Tien';
+    echo 'Biến số 7: '. $bien7. '<br>';
+
+
+    /* 
+        toán tử gán: =, +=, -=, *=, /=, %=, .=;
+        toán tử số học: +, -, *, /, %, **, ++, --;
+        toán tử so sánh: >, <, >=, <=, ==, ===, !=;
+        toán tử lý luận: &&, ||, !    
+    */
 ?>

8911a21b910c1924dd2e44d7d53b0ec5ccc2eeab
congtien2001 <tien1972001@gmail.com>
2024-07-04 20:33:40 +0700

    04/07 html,php

--- PHP/Ex/ex5_toán tử và biểu thức.php
+++ PHP/Ex/ex5_toán tử và biểu thức.php
@@ -1,4 +1,5 @@
 <?php 
+    /* toán tử gán */
     $bien1 = 5;
 
     $bien2 = 6;
@@ -24,6 +25,49 @@
     $bien7 = 'Storm ';
     $bien7 .= 'Nguyen Cong Tien';
     echo 'Biến số 7: '. $bien7. '<br>';
+
+    /* toán tử số học */
+    $bien8 = 25;
+    $bien8 = $bien8 % 20;// dư 5
+    echo 'Biến số 8: '. $bien8. '<br>';
+    //toán tử luỹ thừa
+    $bien9 = 2;
+    $bien10 = 5;
+    $bienTong = $bien9 ** $bien10;
+    echo 'Toán tử luỹ thừa: '.$bienTong.'<br>';
+    //toán tử ++
+    //toán tử --
+
+    /* Toán tử so sánh */
+    $bien10 = 10;
+    $bien11 = "10";
+    //== : so sánh giá trị;
+    echo $bien10 == $bien11;//true
+    echo '<br>';
+    //=== : so sánh tuyệt đối giá trị lẫn dữ liệu
+    echo 'Kết quả: '. ($bien10 === $bien11). '<br>';//flase
+    /* != : so sánh khác nhau */
+    $bien12 = 5;
+    $bien13 = 10;
+    echo 'so sánh khác nhau: '. ($bien12 != $bien13). '<br>';
+
+    /* Toán tử lý luận */
+    //$$: trả về kiểu dữ liệu boolean(true, flase), cùng đúng hoặc cùng sai   
+    $bien14 = 10;
+    $bien15 = 5;
+    $bien16 = 8;
+    echo 'Kết quả &&: '. (($bien14 > $bien15) && ($bien15 < $bien16)). '<br>';
+    //||: toán tử luận trả boolen(true, flase); chỉ cần 1 trong 2 vế đúng là đúng ngược lại
+    echo 'Kết quả ||: '. (($bien14 < $bien15) || ($bien15 < $bien16)). '<br>';
+    //!: phủ định
+    $bien17 = false;
+    echo 'Kết quả !: '. !($bien17). '<br>';
+
+
+
+
+
+
 
 
     /* 
---
+++ PHP/Ex/ex6_biến tăng, biến giảm.php
@@ -0,0 +1,22 @@
+<?php 
+    //biến tăng:
+    $a = 1;
+    $a++;// $a = $a+1
+    //++$a;
+    echo $a. '<br>';
+    //biến giảm:
+    $b = 1;
+    $b--;// $b = $b-1
+    //--$b;
+    echo $b. '<br>';
+
+    /*biến tăng:
+        $a = $a+1;
+        viết tắt: $a++ hoặc ++$a;
+        $a++: thao tác với biến a trước(lấy giá trị) rồi mới tăng lên 1 đơn vị
+        $++a: tăng lên 1 đơn vị trước rồi thao tác vs biến a.
+      biến giảm:
+        $b = $b-1;
+        viết tắt: $b-- hoặc --$b;   
+    */
+?>

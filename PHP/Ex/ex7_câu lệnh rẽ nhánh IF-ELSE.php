3d6c0e8bf536bf26a5e09200b05422e0c5994ccb
congtien2001 <tien1972001@gmail.com>
2024-07-05 11:05:37 +0700

    05/07

--- PHP/Ex/ex7_câu lệnh rẽ nhánh IF-ELSE.php
+++ PHP/Ex/ex7_câu lệnh rẽ nhánh IF-ELSE.php
@@ -1,3 +1,21 @@
 <?php 
+    /* Câu lệnh rẽ nhánh */
+    $bien1 = 10;
+
+    if ($bien1 > 0) {
+        echo'Biến 1 lớn hơn 0';
+    } 
+    else if($bien1 == 5) {
+        echo 'Biến 1 bằng 5';
+    }
+    else if($bien1 ==10) {
+        echo 'Biến 1 lớn nhất';
+    }
+    else {
+        echo 'Trường hợp còn lại';
+    }
+    //if else lồng nhau
+
+
 
 ?>

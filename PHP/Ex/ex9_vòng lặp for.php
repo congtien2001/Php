8c37a90e707c2180bf726e26690c6283b8b594d3
congtien2001 <tien1972001@gmail.com>
2024-07-08 09:54:35 +0700

    08/07

--- PHP/Ex/ex9_vòng lặp for.php
+++ PHP/Ex/ex9_vòng lặp for.php
@@ -1,7 +1,21 @@
 <?php 
         /* vòng lặp for */
    for($i=0; $i<10; $i++){
-       echo $i."<br>";     
+       echo $i."<br>";
+        /* 
+        i=0 in ra 0
+        i=1 in ra 1
+        ...
+        i=9 in ra 9
+        i=10 vì 10 không bé hơn 10 nên dừng
+        */     
+    //BÀI TẬP
+    //Hiển thị số chẵn lẻ trong dãy số từ 1 2 3 ... 100.
+    
+    
+
+
+
    } 
 
 ?>

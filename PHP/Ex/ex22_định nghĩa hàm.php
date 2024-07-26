c07f240ea0c1e1c9a0a45daf7b9abd4c9d6a62c9
congtien2001 <tien1972001@gmail.com>
2024-07-26 21:06:19 +0700

    26/07

---
+++ PHP/Ex/ex22_định nghĩa hàm.php
@@ -0,0 +1,13 @@
+<?php 
+    function makeTotal($a, $b){
+        $tong = $a + $b;
+        echo 'Tổng của a và b: '.$tong;
+    }
+    /* có 2 cách truyền giá trị */
+        //cách 1
+    makeTotal(4, 5);
+        //cách 2
+    // $bien1 = 4;
+    // $bien2 = 5;
+    // makeTotal($bien1, $bien2);
+?>

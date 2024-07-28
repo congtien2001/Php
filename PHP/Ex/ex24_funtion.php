5ac3a3602c3aa0fadadb6901a4a62ce8a4dc99d1
congtien2001 <tien1972001@gmail.com>
2024-07-28 20:54:25 +0700

    28/07

---
+++ PHP/Ex/ex24_funtion.php
@@ -0,0 +1,10 @@
+<?php 
+    //hàm được tạo để gọi riêng cho ex22 cho gọn
+    function makeTotal($a, $b){//hàm không return
+        $tong = $a + $b;
+        echo "Tổng phía sau RETURN là: ".$tong;        
+        return $tong;
+    
+    }
+
+?>
---
+++ PHP/Ex/ex24_hàm return, biến toàn cục, cục bộ và tĩnh.php
@@ -0,0 +1,9 @@
+<?php 
+    include_once"ex24_funtion.php";
+
+    $bien1 = 10;
+    $bien2 = 15;
+
+    $bienTong = makeTotal($bien1, $bien2);
+    echo 'Biến tổng là: '. $bienTong;
+?>

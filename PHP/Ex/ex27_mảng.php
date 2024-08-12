d57c4ad5f99c1db392e68df362c22419d6b15572
congtien2001 <tien1972001@gmail.com>
2024-08-12 21:05:12 +0700

    12/08

--- PHP/Ex/ex27_mảng.php
+++ PHP/Ex/ex27_mảng.php
@@ -3,12 +3,16 @@
     $arr = array('HTML', 'CSS', 'JS');//cách 1
 
     $arr2 = ['key1' => 'HTML', 'key2' => 'CSS', 'key3' => 'JS'];//cách 2
-
-    //thêm phần tử vào mảng
+    //mảng rỗng
+    $arrEmpty = [];
+    
+    //thêm phần tử có key vào mảng
     $arr2['key4'] = 'PHP';
     $arr2['key5'] = 'LARAVEL';
+    //thêm phần tử không key tự tăng
     $arr2[] = 'DEVOPS';
     $arr2[] = 'DOCKER';
+    //thêm thông qua array_push, thêm 3 phần tử vào $arr
     array_push($arr, 'NODEJS', 'C', 'C++');
 
     //in mảng
